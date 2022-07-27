88200f9e8237d3eba8058ae199d78d16694f716e
linhey <is.linhey@outlook.com>
2022-07-27 13:42:28 +0800

    0.0.14

--- Formula/abrarion.rb
+++ Formula/abrarion.rb
@@ -1,8 +1,8 @@
 class Abrarion < Formula
   desc "任务编排工具"
   homepage "https://github.com/autoasset/homebrew-abrarion"
-  url "https://github.com/autoasset/homebrew-abrarion/releases/download/0.0.13/abrarion-0.0.13.tar.gz"
-  sha256 "f4c86f14842c715ba28824d4347bee1604d54e0cfa9cae7d6b91da79c06bed56"
+  url "https://github.com/autoasset/homebrew-abrarion/releases/download/0.0.14/abrarion-0.0.14.tar.gz"
+  sha256 "4435807828f522dc73cfbb2dea2c3587566febfad7894ede29a23cbdd1c677f6"
   def install
     bin.install "abrarion"
   end
