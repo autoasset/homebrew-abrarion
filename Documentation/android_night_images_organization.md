8e210c31707694da800f70aba4613bf92ea2dbc1
linhey <is.linhey@outlook.com>
2022-12-28 14:29:21 +0800

    0.0.34

--- Documentation/android_night_images_organization.md
+++ Documentation/android_night_images_organization.md
@@ -6,5 +6,6 @@
 
 | 字段名       | 描述                     | 类型               | 默认值 | 是否必填 |
 | ------------ | ------------------------ | ------------------ | ------ | -------- |
+| enable | 是否启用该功能 | bool | true | 否 |
 | output_resources_path | android resources 文件夹位置, 用于资源拷贝 | string |  | 是 |
 | is_remove_orphan_file | 是否移除孤儿文件, a.png 与 a_dark.png 必须成对出现, 否则 a_dark.png 将被删除 |   bool | true | 否 |
--- Formula/abrarion.rb
+++ Formula/abrarion.rb
@@ -1,8 +1,8 @@
 class Abrarion < Formula
   desc "任务编排工具"
   homepage "https://github.com/autoasset/homebrew-abrarion"
-  url "https://github.com/autoasset/homebrew-abrarion/releases/download/0.0.33/abrarion-0.0.33.tar.gz"
-  sha256 "8d29a80eac3ccc9de3150b1307ed0550e9e1e8fa334434f8a2dc575ad5069179"
+  url "https://github.com/autoasset/homebrew-abrarion/releases/download/0.0.34/abrarion-0.0.34.tar.gz"
+  sha256 "b7ebdcd7298e930268118b6f75f55ad93161f891472bfbfc98bfc1dc51aa82c3"
   def install
     bin.install "abrarion"
   end
